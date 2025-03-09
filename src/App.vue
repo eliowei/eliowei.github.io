80da2ed680ecc7a2f89a86edf976f6f1565b4ea9
ElioWei <zxc160604@gmail.com>
2025-03-09 23:04:45 +0800

    update App

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <header class="absolute inset-x-0 top-0 z-50">
       <nav class="flex items-center justify-between p-4 lg:px-8" aria-label="Global">
         <div class="flex lg:flex-1">
-          <a href="https://eliowei.github.io/eliowei-portfolio/#/" class="-m-1.5 p-1.5">
+          <a href="#" class="-m-1.5 p-1.5">
             <span>Elio Wei</span>
           </a>
         </div>
@@ -53,7 +53,7 @@
           class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
         >
           <div class="flex items-center justify-between">
-            <a href="#" class="-m-1.5 p-1.5">
+            <a href="#" class="-m-1.5 p-1.5" @click="showMenu = false">
               <span>Elio Wei</span>
             </a>
             <button
@@ -80,11 +80,13 @@
                 <RouterLink
                   to="/"
                   class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  @click="showMenu = false"
                   >關於我</RouterLink
                 >
                 <RouterLink
                   to="/projects"
                   class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  @click="showMenu = false"
                   >作品集</RouterLink
                 >
               </div>
